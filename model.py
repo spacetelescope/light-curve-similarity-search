5c7bd8cbcc048cc66a6ec4c2a0897586747fd40c
Zach Claytor <zclaytor@stsci.edu>
2024-12-16 17:22:53 +0000

    add batchnorm; new run

--- model.py
+++ model.py
@@ -14,39 +14,85 @@ class ConvNet(nn.Module):
 
         super(ConvNet, self).__init__()
         self.conv1 = nn.Conv2d(  1,  c[0], k[0], 1) # 62 x 20
+        self.conv1_bn = nn.BatchNorm2d(c[0])
         self.conv2 = nn.Conv2d(c[0], c[1], k[1], 1) # 60 x 6 
+        self.conv2_bn = nn.BatchNorm2d(c[1])
         self.conv3 = nn.Conv2d(c[1], c[2], k[2], 1) # 58 x 1
+        self.conv3_bn = nn.BatchNorm2d(c[2])
         self.fc1 = nn.Linear(n_nodes, 256) #  58 x 32 = 1856
+        self.fc1_bn = nn.BatchNorm1d(256)
         self.fc2 = nn.Linear(256, 64)
+        self.fc2_bn = nn.BatchNorm1d(64)
         self.fc3 = nn.Linear(64, 4)
         self.dropout1 = nn.Dropout(0.1)
         self.dropout2 = nn.Dropout2d(0.1)
 
     def forward(self, x):
         x = self.conv1(x)
+        x = self.conv1_bn(x)
         x = F.relu(x)
         x = F.max_pool2d(x, (1, 3))
         x = self.dropout2(x)
 
         x = self.conv2(x)
+        x = self.conv2_bn(x)
         x = F.relu(x)
         x = F.max_pool2d(x, (1, 3))
         x = self.dropout2(x)
 
         x = self.conv3(x)
+        x = self.conv3_bn(x)
         x = F.relu(x)
         x = F.max_pool2d(x, (1, x.shape[-1]))
         x = self.dropout2(x)
 
         x = torch.flatten(x, 1)
 
-        x = F.relu(self.fc1(x))
+        x = self.fc1(x)
+        x = self.fc1_bn(x)
+        x = F.relu(x)
         x = self.dropout1(x)
-        x = F.relu(self.fc2(x))
+
+        x = self.fc2(x)
+        x = self.fc2_bn(x)
+        x = F.relu(x)
         x = self.dropout1(x)
+        
         output = self.fc3(x)
         output = F.softmax(output, dim=1)
         return output
+
+    def get_embeddings(self, x):
+        x = self.conv1(x)
+        x = self.conv1_bn(x)
+        x = F.relu(x)
+        x = F.max_pool2d(x, (1, 3))
+        x = self.dropout2(x)
+
+        x = self.conv2(x)
+        x = self.conv2_bn(x)
+        x = F.relu(x)
+        x = F.max_pool2d(x, (1, 3))
+        x = self.dropout2(x)
+
+        x = self.conv3(x)
+        x = self.conv3_bn(x)
+        x = F.relu(x)
+        x = F.max_pool2d(x, (1, x.shape[-1]))
+        x = self.dropout2(x)
+
+        x = torch.flatten(x, 1)
+
+        x = self.fc1(x)
+        x = self.fc1_bn(x)
+        x = F.relu(x)
+        x = self.dropout1(x)
+
+        x = self.fc2(x)
+        x = self.fc2_bn(x)
+        #x = F.relu(x)
+        #x = self.dropout1(x)
+        return x
 
 
 def Gaussian_NLL(y_pred, y_true, k=1, stability_factor=1e-4, reduction='mean'):
