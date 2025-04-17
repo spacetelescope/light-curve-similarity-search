e8fe82916c5366eeb365edccfaa604673b1f8c4e
Zach Claytor <zclaytor@stsci.edu>
2025-04-17 20:22:23 +0000

    tweak EB pipeline

--- catalogs/core.py
+++ catalogs/core.py
@@ -1,3 +1,6 @@
+import os
+import logging
+
 from astroquery.mast import Observations
 from pandas import read_csv
 import numpy as np
@@ -11,9 +14,10 @@ Observations.enable_cloud_dataset() # use cloud data when possible
 fs = s3fs.S3FileSystem(anon=True)   # read cloud data as though local
 
 
-def read_catalog(catname):
+def read_catalog(filepath):
+    logging.info(f"Reading catalog from {filepath}.")
     cat = read_csv(
-        f"catalogs/{catname}.csv", 
+        filepath,
         index_col="TIC", 
         converters={"sectors": lambda x: x.replace("'","").split(",")},
     )
@@ -22,29 +26,38 @@ def read_catalog(catname):
 
 def query_observations(
     catalog,
+    uris_path=None,
     obs_collection="TESS",
     dataproduct_type="timeseries",
     **kw
 ):
     """Query MAST light curves for a specified catalog.
     """
-    ids = catalog.TIC.astype(str)
+    ids = catalog.index.astype(str)
+    logging.info("Querying observations.")
     obs = Observations.query_criteria(
         target_name=ids, 
         obs_collection=obs_collection, 
         dataproduct_type=dataproduct_type, 
         **kw)
 
+    logging.info("Filtering observations.")
     filtered_obs = obs[[str(row["sequence_number"]) in catalog.loc[int(row["target_name"]), "sectors"] for row in obs]]
 
     # Get associated science products for each Observation
+    logging.info("Getting list of data products.")
     data_products = Observations.get_product_list(filtered_obs) 
 
     # Keep only the science products
+    logging.info("Filtering the list of data products.")
     lc_prod = Observations.filter_products(data_products, extension="s_lc.fits", productType="SCIENCE")
 
-    # Query the light curve URIs
+    # Query the light curve URIs, optionally save
+    logging.info("Getting product URIs.")
     lc_uri = Observations.get_cloud_uris(lc_prod)
+    if uris_path:
+        logging.info(f"Saving list of URIs to {uris_path}.")
+        np.savetxt(uris_path, lc_uri, fmt="%s")
 
     return lc_uri
 
@@ -60,7 +73,7 @@ def read_lightcurve(uri, flux_column="sap_flux", **kw):
     Returns:
         lc (lightkurve.LightCurve): the light curve.
     """
-
+    logging.info(f"Reading light curve from {uri}.")
     with fs.open(uri, "rb") as f:
         lc = lightkurve.read(uri, flux_column=flux_column, **kw).remove_nans()
 
@@ -82,7 +95,7 @@ def wavelet_power(
     By default, this computes the Morlet wavelet transform as implemented in scipy.
     Optionally bin the power array to a user-specified shape.
     """
-
+    logging.info("Computing wavelet power spectrum.")
     time = lc.time.value
     flux = lc.flux.value
 
@@ -118,5 +131,35 @@ def wavelet_power(
         power = torch.tensor(np.expand_dims(power, 0))
         power = torch.nn.functional.adaptive_avg_pool2d(power, output_size=output_size)
         power = np.squeeze(power.numpy())
+
+    return power
+
+
+def lc_to_wps(uri, lc_kw=None, wp_kw=None, save_path=None):
+    """Given a URI, read the light curve and compute the wavelet power.
+
+    Wraps `read_lightcurve` and `wavelet_power`.
+
+    Accepts dictionaries `lc_kw` and `wp_kw` for kwargs to the two functions.
+    """
+
+    # Initialize empty dicts if no kwargs are provided
+    if not lc_kw:
+        lc_kw = {}
+    if not wp_kw:
+        wp_kw = {}
+
+    # Read light curve and compute wavelet power spectrum
+    lc = read_lightcurve(uri, **lc_kw)
+    power = wavelet_power(lc, **wp_kw)
+
+    # Rescale power spectrum
+    power -= power.min()
+    power *= 255/power.max()
+    power = power.astype(np.uint8)
+
+    # Optionally save wavelet power spectrum
+    if save_path:
+        np.save(save_path, power)
 
     return power
--- catalogs/eclipsing_binaries.py
+++ catalogs/eclipsing_binaries.py
@@ -1,16 +1,23 @@
-from pandas import read_csv
+import os
+import logging
 
+from pandas import read_csv
+import dask
+import numpy as np
+
+import core
 
 catalog_url = "https://archive.stsci.edu/hlsps/tess-ebs/hlsp_tess-ebs_tess_lcf-ffi_s0001-s0026_tess_v1.0_cat.csv"
 
 
-def read(save=False):
+def read(save_path=None):
     """Download the EB catalog and process it.
 
     Currently sourced from the TESS Eclipsing Binaries Catalog produced by Prša et al. (2022).
     https://archive.stsci.edu/hlsps/tess-ebs
     """
     
+    logging.info(f"Reading catalog from {catalog_url}.")
     ebs = read_csv(catalog_url)
 
     # Remove rows with no sectors
@@ -19,7 +26,76 @@ def read(save=False):
     # Rename columns and save
     ebs = ebs[["tess_id", "sectors"]].rename(columns={"tess_id": "TIC"})
     ebs = ebs.set_index("TIC").sort_index()
-    if save:
-        ebs.to_csv("tess-ebs.csv")
+    if save_path:
+        logging.info(f"Saving catalog to {save_path}.")
+        ebs.to_csv(save_path)
 
     return ebs
+
+
+def process_one(uri, catname):
+    """Process a single light curve.
+    """
+    logging.warning(f"Processing {uri}.")
+    outname = os.path.basename(uri).replace("lc.fits", "wt")
+
+    core.lc_to_wps(uri,
+        lc_kw=dict(flux_column="sap_flux", quality_bitmask="hard"),
+        wp_kw=dict(minimum_period=0.01, maximum_period=12, output_size=64),
+        save_path=f"wavelets/{catname}/{outname}.npy",
+    )
+
+
+def process(catname, load=False):
+    """Process the light curves from the catalog.
+    """
+
+    if load:
+        uris = np.loadtxt(f"catalogs/{catname}-uris.txt", dtype=str)
+    else:
+        # Read the catalog
+        cat = core.read_catalog(f"catalogs/{catname}.csv")
+
+        # Get light curve URIs
+        uris = core.query_observations(cat, uris_path=f"catalogs/{catname}-uris.txt")
+
+    # Process light curves into wavelet power spectra and save
+    lazy_results = []
+    for uri in uris:
+        lazy_result = dask.delayed(process_one)(uri, catname)
+        lazy_results.append(lazy_result)
+    
+    dask.compute(*lazy_results)
+
+
+if __name__ == "__main__":
+    import argparse
+
+    # Create an argument parser
+    parser = argparse.ArgumentParser(description='Set logging level from command line')
+
+    # Add an argument for log level
+    parser.add_argument(
+        '--log',
+        dest='log_level',
+        default='WARNING',
+        choices=['DEBUG', 'INFO', 'WARNING', 'ERROR', 'CRITICAL'],
+        type=str.upper,
+        help='Set the logging level'
+    )
+
+    # Parse the arguments
+    args = parser.parse_args()
+
+    # Get the log level from arguments
+    log_level = args.log_level
+
+    # Set up logger
+    numeric_level = getattr(logging, log_level.upper(), None)
+    if not isinstance(numeric_level, int):
+        raise ValueError('Invalid log level: %s' % log_level)
+    logging.basicConfig(level=numeric_level, format='%(asctime)s %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p')
+
+    catname = "tess-ebs"
+    #read(save_path=f"catalogs/{catname}.csv")
+    process(catname, load=True)
